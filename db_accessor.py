b8ec3c6e64aa592a7546eb59f201f214047ca8bc
troysnowden <67203776+troysnowden@users.noreply.github.com>
2024-02-05 10:47:44 +0000

    feat(nef-4): implement admin home page (#6)
    * feat(nef-4): implement admin home page
    
    * feat(nef-4): implement admin home page
    
    * feat: add qr code add reward functionality
    
    * feat(nef-4): add admin account page
    
    * feat(nef-4): implement admin account
    
    ---------
    
    Co-authored-by: Troy Snowden <troy.snowden@engineering.digital.dwp.gov.uk>

--- db_accessor.py
+++ db_accessor.py
@@ -1,20 +1,42 @@
+usersData = {
+    "student1": {
+        "name": "John Doe",
+        "password": "password1",
+        "is_student": True,
+        "num_rewards": 0
+    }, 
+    "student2": {
+        "name": "Jane Doe",
+        "password": "password2",
+        "is_student": True,
+        "num_rewards": 1
+    }, 
+    "admin3": {
+        "name": "Admin Doe",
+        "password": "password3",
+        "is_student": False
+    }
+}
+
+def increment_student_rewards(username):
+    usersData[username]["num_rewards"] += 1
+
 def push_user_to_db(username, password):
     # not yet implemented
 
     return True
 
-def is_user_registered(username, password):
-    registered_users = get_registered_users()
+# def is_user_registered(username, password):
+#     registered_users = get_registered_users()
 
-    if registered_users.get(username) == password:
-        return True
-    else:
-        return False
+#     user = registered_users.get(username)
+
+#     if user and user.get("password") == password:
+#         return True
+#     else:
+#         return False
 
 
 def get_registered_users():
     #key = username, value = password
-    return {
-        "student1": "password1",
-        "admin2": "password2"
-    }
+    return usersData
--- helpers.py
+++ helpers.py
@@ -9,7 +9,9 @@
 # - good variable/method names
 # - code duplication
 
-from db_accessor import push_user_to_db, get_registered_users
+from db_accessor import push_user_to_db, get_registered_users, increment_student_rewards
+from flask import session, redirect
+from functools import wraps
 
 
 def register_user(username, password, confirmed_password):
@@ -18,6 +20,39 @@ def register_user(username, password, confirmed_password):
         return push_user_to_db(username, password)
     else:
         return False
+
+def login_user(username, password):
+    user = get_registered_users().get(username)
+    
+    if user and user.get("password") == password:
+        return True
+    else:
+        return False
+
+def add_student_reward(username):
+    user = get_registered_users().get(username)
+
+    if user:
+        return increment_student_rewards(username)
+    else:
+        return False
+
+def get_logged_in_user_data(username):
+    return get_registered_users().get(username)
+
+def is_student(username):
+    user = get_registered_users().get(username)
+    return user and user.get("is_student")
+
+def get_student_users():
+    users = get_registered_users()
+    student_users = {}
+
+    for username, data in users.items():
+        if data["is_student"]:
+            student_users[username] = data
+    
+    return student_users
 
 def check(password):
     if len(password) < 5:
@@ -42,16 +77,15 @@ def check(password):
     
     return containsSpecialChar
 
+def login_required(f):
+    """
+    Decorate routes to require login.
 
-def login_user(username, password):
-    registered_users = get_registered_users()
-    
-    if registered_users.get(username) == password:
-        return True
-    else:
-        return False
-
-
-
-def is_admin(username):
-    return username == "admin2"
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("username") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
